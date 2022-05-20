8bcc2e3a668210ea90a74b17b3e876323ca1298f
ankerx <93661548+ankerx@users.noreply.github.com>
2022-05-20 11:07:19 +0200

    added weather api

--- src/modules/Maps/Details/Details.js
+++ src/modules/Maps/Details/Details.js
@@ -2,7 +2,8 @@ import { jsPDF } from "jspdf";
 import SaveModal from "../../Home/Trips/Modal/SaveModal";
 import { Link } from "react-router-dom";
 import styles from "./details.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Details({
   roadTime,
@@ -12,6 +13,25 @@ function Details({
   values,
   setDetails,
 }) {
+  const [temp, setTemp] = useState("");
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      url: "https://weatherapi-com.p.rapidapi.com/current.json",
+      params: { q: values.secondDestination },
+      headers: {
+        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+        "X-RapidAPI-Key": "4ad9038353msh63669962654b11ap143e63jsnc4f9649b2c64",
+      },
+    };
+    axios
+      .request(options)
+      .then((res) => setTemp(res.data.current.temp_c))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [values.secondDestination]);
+
   const [saved, setSaved] = useState(false);
   const handleChange = (event) => {
     setPrice(event.target.value);
@@ -40,9 +60,16 @@ function Details({
       setSaved(false);
     }, 3000);
   };
+  const Capitalize = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
   return (
     <div className={styles.container} id="pdf">
       <h2>Details:</h2>
+      <p>
+        Current temperature in {Capitalize(values.secondDestination)}: {temp}
+        °C
+      </p>
       <p>Distance: {roadLength.toFixed(1)} km</p>
       <p>Time: {roadTime.toFixed(0)} minutes</p>
       <form className={styles.form}>
